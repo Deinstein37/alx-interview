826712605dde5523f3d0eb831074b2c2585d55a7
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2023-02-22 10:44:26 +0100

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,52 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
 """
-Define validUTF8(data) function that validates whether a
-string of ints represents a valid UTF-8 encoding.
+0-simple_helper_function
 """
-from itertools import takewhile
 
 
-def int_to_bits(nums):
+def index_range(page: int, page_size: int) -> tuple:
     """
-    Helper function
-    Convert ints to bits
+    Takes two integer arguments page and page_size.
+    Returns a tuple of size two containing a start
+    index and an end index corresponding to the range
+    of indexes to return in a list for those particular
+    pagination parameters.
     """
-    for num in nums:
-        bits = []
-        mask = 1 << 8  # cause we have 8 bits per byte. adds up to (11111111)
-        while mask:
-            mask >>= 1
-            bits.append(bool(num & mask))
-        yield bits
-
-
-def validUTF8(data):
-    """
-    Takes a list of ints and returns true if the list is
-    a valid UTF-8 encoding, else returns false
-    Args:
-        data : List of ints representing possible UTF-8 encoding
-    Return:
-        bool : True or False
-    """
-    bits = int_to_bits(data)
-    for byte in bits:
-        # if single byte char, then valid. continue
-        if byte[0] == 0:
-            continue
-
-        # if here, byte is multi-byte char
-        ones = sum(takewhile(bool, byte))
-        if ones <= 1:
-            return False
-        if ones >= 4:  # UTF-8 can be 1 to 4 bytes long
-            return False
-
-        for _ in range(ones - 1):
-            try:
-                byte = next(bits)
-            except StopIteration:
-                return False
-            if byte[0:2] != [1, 0]:
-                return False
-    return True
+    index_tuple = page_size * (page - 1), page * pag-----------------------------------------------------------
